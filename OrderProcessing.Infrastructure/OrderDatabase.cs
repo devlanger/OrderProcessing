169e0ce2a8dcdcad904f6b9180a54399d1b6284b
langer <langer.msg@gmail.com>
2025-11-05 23:26:09 +0100

    Feature: Add In-memory database

---
+++ OrderProcessing.Infrastructure/OrderDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+
+namespace OrderProcessing.Infrastructure;
+
+public class OrderDatabase
+{
+    public ConcurrentDictionary<int, Order> Orders { get; set; }
+
+    public void SeedData()
+    {
+        Orders = new ConcurrentDictionary<int, Order>()
+        {
+            Values =
+            {
+                { new Order() { Id = 1, Description = "Laptop"} },
+                { new Order() { Id = 2, Description = "Phone"} }
+            }
+        };
+    }
+}
