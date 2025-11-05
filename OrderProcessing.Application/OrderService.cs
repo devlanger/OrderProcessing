16530e8e5f52eeb7865659ce11d57bd9e0af2124
langer <langer.msg@gmail.com>
2025-11-05 23:27:14 +0100

    Feature: Add OrderRepository and OrderService implementation

---
+++ OrderProcessing.Application/OrderService.cs
@@ -0,0 +1,23 @@
+﻿using OrderProcessing.Abstractions;
+
+namespace OrderProcessing.Application;
+
+public class OrderService(IOrderRepository orderRepository, ILogger logger) : IOrderService
+{
+    public Task ProcessOrder(int orderId)
+    {
+        logger.LogInfo($"Start processing order: {orderId} ...");
+
+        try
+        {
+            orderRepository.GetOrder(orderId);
+        }
+        catch (Exception e)
+        {
+            logger.LogError($"Error while processing order: {orderId}", e);
+            throw;
+        }
+        
+        return Task.CompletedTask;
+    }
+}
---
+++ OrderProcessing.Infrastructure/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using OrderProcessing.Abstractions;
+
+namespace OrderProcessing.Infrastructure;
+
+public class OrderRepository(OrderDatabase database) : IOrderRepository
+{
+    public string GetOrder(int orderId)
+    {
+        if(orderId <= 0)
+            throw new ArgumentException("Invalid order id", nameof(orderId));
+
+        if (database.Orders.TryGetValue(orderId, out var order))
+            return order.Description;
+
+        throw new KeyNotFoundException($"Order with id {orderId} not found");
+    }
+}
