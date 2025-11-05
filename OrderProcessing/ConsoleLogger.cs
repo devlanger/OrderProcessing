0ebc7094202f113422d0f6f78cffa4e79fde1ece
langer <langer.msg@gmail.com>
2025-11-05 23:05:40 +0100

    Feature: Add logger abstraction and implementation

---
+++ OrderProcessing/ConsoleLogger.cs
@@ -0,0 +1,14 @@
+﻿public class ConsoleLogger : ILogger
+{
+    private static DateTimeOffset GetLogDateTime() => DateTimeOffset.UtcNow;
+
+    public void LogInfo(string message)
+    {
+        Console.WriteLine($"[{GetLogDateTime()}] - {message}");
+    }
+
+    public void LogError(string message, Exception ex)
+    {
+        Console.WriteLine($"[{GetLogDateTime()}] - {message}");
+    }
+}
---
+++ OrderProcessing/ILogger.cs
@@ -0,0 +1,5 @@
+﻿public interface ILogger
+{
+    void LogInfo(string message);
+    void LogError(string message, Exception ex);
+}
