ef4f3e2c44e44058d3b2a19f077fe87abb7e6c9c
langer <langer.msg@gmail.com>
2025-11-06 09:50:54 +0100

    Tests: Add unit tests for Order Service

---
+++ OrderProcessing.Tests/OrderServiceTests.cs
@@ -0,0 +1,74 @@
+using Moq;
+using OrderProcessing.Abstractions;
+using OrderProcessing.Application;
+
+namespace OrderProcessing.Tests;
+
+public class OrderServiceTests
+{
+    private readonly Mock<IOrderRepository> _orderRepositoryMock;
+    private readonly Mock<ILogger> _loggerMock;
+    private readonly IOrderService _sut;
+
+    public OrderServiceTests()
+    {
+        _orderRepositoryMock = new Mock<IOrderRepository>();
+        _loggerMock = new Mock<ILogger>();
+        
+        _sut = new OrderService(_orderRepositoryMock.Object, _loggerMock.Object);
+    }
+    
+    [Fact]
+    public async Task ProcessOrderAsync_ValidOrder_ShouldLogAndCallRepository()
+    {
+        //Arrange
+        const int orderId = 1;
+        
+        _orderRepositoryMock
+            .Setup(x => x.GetOrderAsync(orderId))
+            .ReturnsAsync("Test description");
+        
+        //Act
+        await _sut.ProcessOrderAsync(orderId);
+        
+        //Assert
+        _orderRepositoryMock.Verify(x => x.GetOrderAsync(orderId), Times.Once);
+        _loggerMock.Verify(x => x.LogInfo(It.IsAny<string>()), Times.Once);
+        _loggerMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task ProcessOrderAsync_InvalidOrder_ShouldThrowAndLogError()
+    {
+        const int orderId = -1;
+        _orderRepositoryMock
+            .Setup(r => r.GetOrderAsync(orderId))
+            .ThrowsAsync(new ArgumentException());
+        
+        //Act
+        await Assert.ThrowsAsync<ArgumentException>(async () => await _sut.ProcessOrderAsync(orderId));
+        
+        //Assert
+        _orderRepositoryMock.Verify(x => x.GetOrderAsync(orderId), Times.Once);
+        _loggerMock.Verify(x => x.LogInfo(It.IsAny<string>()), Times.Once);
+        _loggerMock.Verify(x => x.LogError(It.IsAny<string>(), It.IsAny<Exception>()), Times.Once);
+    }
+    
+    
+    [Fact]
+    public async Task ProcessOrderAsync_InvalidOrder_NotFound()
+    {
+        const int orderId = 999;
+        _orderRepositoryMock
+            .Setup(r => r.GetOrderAsync(orderId))
+            .ThrowsAsync(new KeyNotFoundException());
+        
+        //Act
+        await Assert.ThrowsAsync<KeyNotFoundException>(async () => await _sut.ProcessOrderAsync(orderId));
+        
+        //Assert
+        _orderRepositoryMock.Verify(x => x.GetOrderAsync(orderId), Times.Once);
+        _loggerMock.Verify(x => x.LogInfo(It.IsAny<string>()), Times.Once);
+        _loggerMock.Verify(x => x.LogError(It.IsAny<string>(), It.IsAny<Exception>()), Times.Once);
+    }
+}
